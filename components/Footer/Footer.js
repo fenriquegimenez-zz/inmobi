e67786b9a9f2d4fe99b04e88e96363511fa67cfc
Enrique Gimenez <fenriquegimenez@gmail.com>
2021-05-11 17:40:35 -0400

    source code repository link added to footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,7 +3,15 @@ import React from "react"
 const Footer = () => {
   return (
     <div className="text-center my-3 bg-light py-3">
-      <small className="text-secondary">Inmobi ®. All rights reserved.</small>
+      <small className="text-secondary">
+        Inmobi ®. All rights reserved. View the source code on{" "}
+        <a
+          className="text-decoration-none text-dark"
+          href="https://github.com/fenriquegimenez/inmobi"
+        >
+          Github
+        </a>
+      </small>
     </div>
   )
 }
